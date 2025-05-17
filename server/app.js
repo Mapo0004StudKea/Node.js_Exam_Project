876029feaaba9470492f233df85294095832940f
Martin Poulsen <mapo0004@stud.kea.dk>
2025-05-17 10:39:20 +0200

    minor update
    added sessions to app.js
    created a random string for SESSION_SECRET in the .env file

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import 'dotenv/config';
+import session from 'express-session'; // Importer express-session
 
 const app = express();
 
 app.use(express.json());
-
 app.use(express.static("public"));
+
+// Konfigurer express-session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'enhemmeligstreng', // Her konfigureres secret optionen korrekt
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24
+  }
+}));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
